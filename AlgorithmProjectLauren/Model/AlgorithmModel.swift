ccc310100748157aa27bac5fc558a73e5d0a9342
Davis <ldav7984@740s-pl-109.local>
2018-11-15 12:23:41 -0700

    Created a model class.

---
+++ AlgorithmProjectLauren/Model/AlgorithmModel.swift
@@ -0,0 +1,9 @@
+//
+//  AlgorithmModel.swift
+//  AlgorithmProjectLauren
+//
+//  Created by Davis, Lauren on 11/15/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import Foundation
