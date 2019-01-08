45b7dbed5016efb1edb6370049ba7781dd7a2747
Davis <ldav7984@740s-pl-109.local>
2019-01-08 13:54:10 -0700

    Removed an empty file.

--- AlgorithmProjectLauren/Model/AlgorithmModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AlgorithmModel.swift
-//  AlgorithmProjectLauren
-//
-//  Created by Davis, Lauren on 11/15/18.
-//  Copyright © 2018 Davis, Lauren. All rights reserved.
-//
-
-import Foundation
